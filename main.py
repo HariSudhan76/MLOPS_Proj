a4180afb17aeb24a032b82af869eecb920cae940
HariSudhan76 <g.harisudhan76@gmail.com>
2025-04-23 13:46:29 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation, DataValidationConfig
 from networksecurity.exception.exception import CustomException
 from networksecurity.exception.customlogging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
@@ -11,9 +12,16 @@ if __name__=='__main__':
         tpc = TrainingPipelineConfig()
         dic = DataIngestionConfig(tpc)
         di = DataIngestion(dic)
-        logging.info("Enter the try block")
+        logging.info("Initiate the data ingestion")
         dia = di.initiate_data_ingestion()
+        logging.info("Data initiation Completed")
+        dvc = DataValidationConfig(tpc)
+        dv = DataValidation(dia,dvc)
+        logging.info("Initiate the data validation")
+        dva = dv.initiate_data_validation()
         print(dia)    
+        print(dva)
+        logging.info("data validation completed")
     except Exception as e:
         raise CustomException(e,sys)
 
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -14,4 +14,16 @@ def read_yaml_file(file_path: str) -> dict:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise CustomException(e,sys) from e
-        
+
+def write_yaml_file(file_path: str, content: object, replace: bool=False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)        
+        with open(file_path,"w") as file:
+            yaml.safe_dump(content, file,default_flow_style=False, 
+            allow_unicode=True, 
+            sort_keys=False)
+    except Exception as e:
+        raise CustomException(e,sys)
