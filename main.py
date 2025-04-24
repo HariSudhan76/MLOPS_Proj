66338bb0eba9b73b77e7ed711ab098595895477e
HariSudhan76 <g.harisudhan76@gmail.com>
2025-04-24 09:58:10 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation, DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import CustomException
 from networksecurity.exception.customlogging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataTransformationConfig
 
 if __name__=='__main__':
     try:
@@ -14,14 +15,23 @@ if __name__=='__main__':
         di = DataIngestion(dic)
         logging.info("Initiate the data ingestion")
         dia = di.initiate_data_ingestion()
+        # print(dia)    
         logging.info("Data initiation Completed")
+
         dvc = DataValidationConfig(tpc)
         dv = DataValidation(dia,dvc)
         logging.info("Initiate the data validation")
         dva = dv.initiate_data_validation()
-        print(dia)    
-        print(dva)
+        # print(dva)
         logging.info("data validation completed")
+
+
+        dtc = DataTransformationConfig(tpc)
+        dt = DataTransformation(dva,dtc)
+        logging.info("Initiate the data transformation")
+        dta = dt.initiate_data_transformation()
+        print(dta)
+        logging.info("data transformation completed")
     except Exception as e:
         raise CustomException(e,sys)
 
